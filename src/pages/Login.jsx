de0cd5948974694d98ba24ab26c2b1821f82ef02
dyashkumar146 <dyashkumar146@gmail.com>
2026-08-18 17:36:41 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,8 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://careerbridge-backend-1bd5.onrender.com
+', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
