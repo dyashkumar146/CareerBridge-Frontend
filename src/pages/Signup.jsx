1bb6bfb507f68bcb3bfa992591ee07fd8bfb2a1b
dyashkumar146 <dyashkumar146@gmail.com>
2026-08-19 22:54:09 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://careerbridge-backend-1bd5.onrender.com/api/auth/register',{
+      const response = await fetch('https://careerbridge-backend-1bd5.onrender.com/api/auth/signup',{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
